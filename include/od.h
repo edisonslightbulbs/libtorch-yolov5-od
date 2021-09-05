f912d44836a50fd5b219db34f83b82adef21aad7
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-09-05 13:38:45 +0200

    introducing object_t type

--- include/od.h
+++ include/od.h
@@ -9,12 +9,13 @@
 #include <vector>
 
 #include "i3d.h"
+#include "object.h"
 
 namespace od {
 std::vector<torch::Tensor> nonMaxSuppression(
     torch::Tensor& preds, float scoreThresh = 0.5, float iouThresh = 0.5);
 
-std::vector<std::pair<cv::Rect, std::vector<std::string>>> detect(const int& h, const int& w, uint8_t* bgraData,
+std::vector<object_t> detect(const int& h, const int& w, uint8_t* bgraData,
     std::vector<std::string>& classnames, torch::jit::script::Module& module,
     std::shared_ptr<i3d>& sptr_i3d);
 
