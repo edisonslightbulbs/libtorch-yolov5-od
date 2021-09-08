be1a94b32526e6601a6ba94623232a8ca4cef9c9
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-09-08 13:24:21 +0200

    changing detect function interface

--- include/od.h
+++ include/od.h
@@ -15,11 +15,11 @@ namespace od {
 std::vector<torch::Tensor> nonMaxSuppression(
     torch::Tensor& preds, float scoreThresh = 0.5, float iouThresh = 0.5);
 
-std::vector<object_t> detect(const int& h, const int& w, uint8_t* bgraData,
-    std::vector<std::string>& classnames, torch::jit::script::Module& module,
-    std::shared_ptr<i3d>& sptr_i3d);
-
 void setup(std::vector<std::string>& classNames, torch::jit::Module& module, const std::string& torchscript,
     const std::string& classnames);
+
+std::vector<object_t>
+detect(const int& h, const int& w, cv::Mat& frame, std::vector<std::string>& classnames, torch::jit::Module& module,
+    std::shared_ptr<i3d>& sptr_i3d);
 }
 #endif //LIBTORCH_YOLOV5_OD_H
